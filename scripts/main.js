74d08d577099cddc7ab173a8fcc412a4230c9823
AmirGorbachev <amir.gorbachyow@yandex.ru>
2018-01-09 14:08:06 +0600

    UPD
    UPD

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,6 @@
 	var App = window.App;
 	var Truck = App.Truck;
 	var DataStore = App.DataStore;
-	var myTruck = new Truck('ncc-1701', new DataStore());
+	var myTruck = new Truck('Coffee', new DataStore());
+	window.myTruck = myTruck;
 })(window);
